26f4dd1a9201d4e7054d217d112d1e7790df7e65
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2022-01-06 20:11:58 -0500

    more

---
+++ cggrep.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+import utils
+
+parser = argparse.ArgumentParser()
+parser.add_argument('book', action='store')
+parser.add_argument('query', action='store')
+parser.add_argument('-i', action='store_true')
+args = parser.parse_args()
+
+n = 1
+cur = []
+pat = re.compile(args.query)
+
+ct = 0
+
+with open(f'{args.book}.parsed.cg3.txt') as fin:
+    for l in fin:
+        if not l.strip():
+            blk = '\n'.join(cur)
+            if pat.search(blk):
+                if not args.i:
+                    print('----------')
+                print(n)
+                if not args.i:
+                    print(blk)
+                ct += 1
+            cur = []
+            n += 1
+        else:
+            cur.append(l.rstrip())
+
+print('----------')
+print(f'Showing {ct} matches')

fefdf62d07ba89fcc541da26fe639ee8538ed219
Daniel Swanson <awesomeevildudes@gmail.com>
2025-04-03 09:52:12 -0400

    fix book adding process

--- add_book.sh
+++ add_book.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 python3 splitting.py $1
-make "temp/plain-cg3/$1.txt"
-make "temp/parsed-cg3/$1.txt"
-make "temp/merged/$1.conllu"
-touch "data/manual/$1.conllu"
+touch "data/manual-arcs/$1.tsv"
+touch "data/manual-heads/$1.tsv"
+make "temp/macula-arcs/$1.tsv"
+make "temp/macula-cg3/$1.txt"
+make "temp/macula-parsed-cg3/$1.txt"
+make "temp/macula-merged/$1.conllu"
 touch "data/checked/$1.conllu"
 touch "data/checkable/$1.conllu"
 touch "data/incomplete/$1.conllu"
